226f465a2314dd1d97073919cf9e591d691b1f4e
Charlotte Mothersole <charlottemothersole@gmail.com>
2025-10-06 11:29:17 +0100

    exercise 2a

--- exercises/2a.sql
+++ exercises/2a.sql
@@ -1,3 +1,5 @@
 -- Retrieve a list of all albums
 -- Sort by their release date, with the newest album first
 -- Include all columns
+.mode markdown
+select * from albums order by release_date DESC;
